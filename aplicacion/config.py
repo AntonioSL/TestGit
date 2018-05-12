1d82e58ed48425a4dec870d25858445ddedf5e6e
AntonioSL <l.antonyo.al@gmail.com>
2018-05-11 22:14:45 -0500

    new
    new

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -5,7 +5,8 @@ PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://josedom24:usuario1234@josedom24.mysql.pythonanywhere-services.com/josedom24$tienda'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LuisSL.mysql.pythonanywhere-services.com/LuisSL$tienda
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LuisSL:usuario1234@LuisSL.mysql.pythonanywhere-services.com/LuisSL$tienda2'
+
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
