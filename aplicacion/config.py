0a3de260662a2849e911d225854e70413d805f5a
AntonioSL <l.antonyo.al@gmail.com>
2018-05-13 02:33:17 -0500

    nnn
    nnn

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -4,8 +4,8 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://josedom24:usuario1234@josedom24.mysql.pythonanywhere-services.com/josedom24$tienda'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LuisSL:admin123@LuisSL.mysql.pythonanywhere-services.com/LuisSL$tienda'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://josedom24:usuario1234@josedom24.mysql.pythonanywhere-services.com/josedom24$tienda'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LuisSL:admin123@LuisSL.mysql.pythonanywhere-services.com/LuisSL$tienda'
 
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
